07837562341076f9a3130b0f45f82bb7834598cb
Serge Toro <sombr@users.noreply.github.com>
2023-08-13 15:55:33 +0100

    add rust makefiles and remove perl from the runner (who`s interested in Perl anyway, common Serge)

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all:
 	cd c-baseline && make
 	cd golang && make
+	cd golang-builtin && make
 	cd java && make
+	cd rust && make
+	cd rust-builtin && make
 
 run: all
 	perl run.pl | tee results.txt
--- run.pl
+++ run.pl
@@ -7,13 +7,17 @@ my $binaries = {
     "GCC" => "c-baseline/baseline-gcc",
     "CLANG" => "c-baseline/baseline-clang",
     "GO" => "golang/bench-go",
+    "GO-BUILTIN" => "golang-builtin/bench-go",
     "JAVA" => "cd java && java Main",
     "JS" => "cd javascript && node main.js",
     "PYTHON" => "cd python && python3 main.py",
-    "PYPY" => "cd python && pypy3 main.py"
+    "PYPY" => "cd python && pypy3 main.py",
+    "RUST" => "rust/target/release/rust 10000 5000000",
+    "RUST-BUILTIN" => "rust-builtin/target/release/rust 10000 5000000",
+    #    "PERL" => "cd perl && perl main.pl"
 };
 
-for my $idx (1..4096) {
+for my $idx (1..1024) {
     for my $prefix (keys %$binaries) {
         my $cmd = $binaries->{$prefix};
 
